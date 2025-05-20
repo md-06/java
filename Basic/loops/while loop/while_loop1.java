d568044901d72642ffb899cc15f5c94c306f7d5b
Mohammad Ali <141406050+md-06@users.noreply.github.com>
2025-05-20 23:15:38 +0530

    while_loop1.java

---
+++ Basic/loops/while loop/while_loop1.java
@@ -0,0 +1,11 @@
+// while loop 
+
+public class while_loop1{
+    public static void main(String[] args) {
+        int i=5;
+        while(i<10)
+        System.out.println("i= "+(++i));//op i= 6,7,8,9,10 
+    }
+    return 0;
+    
+}
