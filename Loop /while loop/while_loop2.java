5a6f38993ef78fcf8f83fd9537cd5a72b02fa078
Mohammad Ali <141406050+md-06@users.noreply.github.com>
2025-05-20 23:03:18 +0530

    while_loop2.java

---
+++ Loop /while loop/while_loop2.java
@@ -0,0 +1,14 @@
+// while loop 
+
+public class while_loop2{
+    public static void main(String[] args) {
+        int i=5;
+        while(i<10){
+        i++;
+        System.out.println("i= "+(++i));//op i= 7,9
+        
+        }
+    }
+    return 0;
+    
+}
