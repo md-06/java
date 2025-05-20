38d5869a227209a67d63eb736909a37e678804fe
Mohammad Ali <141406050+md-06@users.noreply.github.com>
2025-05-20 23:17:37 +0530

    while_loop3.java

---
+++ Basic/loops/while loop/while_loop3.java
@@ -0,0 +1,15 @@
+// calculating sum using while loop
+
+public class while_loop3{
+    public static void main(String[] args) {
+        int i=3,sum=0;
+        while(i<=10){
+          sum+=i;
+          i++
+        
+        }
+      System.out.println("sum= "+sum);//o/p: sum=52
+    }
+    return 0;
+    
+}
